f3dc459e79d81ded4671757fa31b9d4b53de3e3b
Siddhanta <66236925+siddhantac@users.noreply.github.com>
2025-08-08 13:54:51 +0800

    use correct syntax

--- boards/shields/nice_view_gem/widgets/layer.c
+++ boards/shields/nice_view_gem/widgets/layer.c
@@ -6,9 +6,7 @@ void draw_layer_status(lv_obj_t *canvas, const struct status_state *state) {
     lv_draw_label_dsc_t label_dsc;
     init_label_dsc(&label_dsc, LVGL_FOREGROUND, &pixel_operator_mono, LV_TEXT_ALIGN_CENTER);
 
-    char text[10] = {};
-
-    text = "POTKA";
+    char text[10] = "POTKA";
 
     // if (state->layer_label == NULL) {
     //     sprintf(text, "Layer %i", state->layer_index);
