6a0e3e6b2408e5794ed9e68cd90e9f719137d3d2
Siddhanta <66236925+siddhantac@users.noreply.github.com>
2025-09-07 15:24:37 +0800

    update layer text

--- boards/shields/nice_view_gem/widgets/layer.c
+++ boards/shields/nice_view_gem/widgets/layer.c
@@ -8,10 +8,11 @@ void draw_layer_status(lv_obj_t *canvas, const struct status_state *state) {
 
     char text[10] = {};
 
-    if (state->layer_label == NULL) { // never true
-        sprintf(text, "Layer %i", state->layer_index);
+    if (state->layer_label == NULL || strlen(state->layer_label) == 0) {
+        sprintf(text, "Layer: %i", state->layer_index);
     } else {
-        sprintf(text, "Layer %i %s", state->layer_index, state->layer_label); // state->layer_label is an empty string
+        strncpy(text, state->layer_label, 9);
+        to_uppercase(text);
     }
 
     lv_canvas_draw_text(canvas, 0, 146 + BUFFER_OFFSET_BOTTOM, 68, &label_dsc, text);
