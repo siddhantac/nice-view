0ecfc451cf78c95f9c03b769e219dade1f8bed8e
Siddhanta <66236925+siddhantac@users.noreply.github.com>
2025-09-07 12:39:30 +0800

    clean up layer label code

--- boards/shields/nice_view_gem/widgets/layer.c
+++ boards/shields/nice_view_gem/widgets/layer.c
@@ -6,17 +6,12 @@ void draw_layer_status(lv_obj_t *canvas, const struct status_state *state) {
     lv_draw_label_dsc_t label_dsc;
     init_label_dsc(&label_dsc, LVGL_FOREGROUND, &pixel_operator_mono, LV_TEXT_ALIGN_CENTER);
 
-    // char text[10] = "POTKA";
     char text[10] = {};
 
-    if (state->layer_label == NULL) {
-        // sprintf(text, "Lyr %i", state->layer_index);
-        strncpy(text, "NULL", 4);
+    if (state->layer_label == NULL) { // never true
+        sprintf(text, "Layer %i", state->layer_index);
     } else {
-        // strncpy(text, state->layer_label, 5);
-        sprintf(text, "L %i %s", state->layer_index, state->layer_label);
-        // strncpy(text, "POTKA", 5);
-        // to_uppercase(text);
+        sprintf(text, "Layer %i %s", state->layer_index, state->layer_label); // state->layer_label is an empty string
     }
 
     lv_canvas_draw_text(canvas, 0, 146 + BUFFER_OFFSET_BOTTOM, 68, &label_dsc, text);
