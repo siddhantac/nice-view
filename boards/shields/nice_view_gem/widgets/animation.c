708ece88f274471c1c9f591e991629b031751de5
Siddhanta <66236925+siddhantac@users.noreply.github.com>
2025-09-17 16:51:40 +0800

    add 16 frames for arasaka glitch animation

--- boards/shields/nice_view_gem/widgets/animation.c
+++ boards/shields/nice_view_gem/widgets/animation.c
@@ -27,7 +27,10 @@ LV_IMG_DECLARE(Arasaka_Logo_glitch_1);
 LV_IMG_DECLARE(Arasaka_Logo_glitch_2);
 
 const lv_img_dsc_t *arasaka_anim[] = {
-    &Arasaka_Logo, &Arasaka_Logo_glitch_1, &Arasaka_Logo_glitch_2,
+    &Arasaka_Logo, &Arasaka_Logo, &Arasaka_Logo, &Arasaka_Logo, 
+    &Arasaka_Logo_glitch_1, &Arasaka_Logo_glitch_1, &Arasaka_Logo_glitch_1, &Arasaka_Logo_glitch_1, 
+    &Arasaka_Logo_glitch_2, &Arasaka_Logo_glitch_2, &Arasaka_Logo_glitch_2, &Arasaka_Logo_glitch_2,
+    &Arasaka_Logo, &Arasaka_Logo, &Arasaka_Logo, &Arasaka_Logo, 
 };
 
 const lv_img_dsc_t *anim_imgs[] = {
@@ -41,7 +44,8 @@ void draw_animation(lv_obj_t *canvas) {
     lv_obj_t *art = lv_animimg_create(canvas);
     lv_obj_center(art);
 
-    lv_animimg_set_src(art, (const void **)anim_imgs, 16);
+    // lv_animimg_set_src(art, (const void **)anim_imgs, 16);
+    lv_animimg_set_src(art, (const void **)arasaka_anim, 16);
     lv_animimg_set_duration(art, CONFIG_NICE_VIEW_GEM_ANIMATION_MS);
     lv_animimg_set_repeat_count(art, LV_ANIM_REPEAT_INFINITE);
     lv_animimg_start(art);
