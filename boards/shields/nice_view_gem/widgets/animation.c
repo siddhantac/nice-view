10154d73a84db0e539aed5fe2a65fbdbbc48a25d
Siddhanta <66236925+siddhantac@users.noreply.github.com>
2025-09-18 14:15:27 +0800

    comment alignment

--- boards/shields/nice_view_gem/widgets/animation.c
+++ boards/shields/nice_view_gem/widgets/animation.c
@@ -57,5 +57,5 @@ void draw_animation(lv_obj_t *canvas) {
     lv_img_set_src(art, &arasaka);
 #endif
 
-    lv_obj_align(art, LV_ALIGN_TOP_LEFT, 36, 0);
+    // lv_obj_align(art, LV_ALIGN_TOP_LEFT, 36, 0);
 }
