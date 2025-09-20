f95038c6fd22b8bed04c592d0eed336ca4523c11
Siddhanta <66236925+siddhantac@users.noreply.github.com>
2025-09-20 22:24:32 +0800

    set 24frames

--- boards/shields/nice_view_gem/widgets/animation.c
+++ boards/shields/nice_view_gem/widgets/animation.c
@@ -17,6 +17,10 @@ const lv_img_dsc_t *arasaka_anim[] = {
     &arasaka, &arasaka,
     &arasaka, &arasaka,
     &arasaka, &arasaka,
+    &arasaka, &arasaka,
+    &arasaka, &arasaka,
+    &arasaka, &arasaka,
+    &arasaka, &arasaka,
     &arasaka_glitch_1, 
     &arasaka_glitch_2, 
     &arasaka_glitch_3, 
@@ -35,7 +39,7 @@ void draw_animation(lv_obj_t *canvas) {
     lv_obj_center(art);
 
     // lv_animimg_set_src(art, (const void **)anim_imgs, 16);
-    lv_animimg_set_src(art, (const void **)arasaka_anim, 16);
+    lv_animimg_set_src(art, (const void **)arasaka_anim, 24);
     lv_animimg_set_duration(art, CONFIG_NICE_VIEW_GEM_ANIMATION_MS);
     lv_animimg_set_repeat_count(art, LV_ANIM_REPEAT_INFINITE);
     lv_animimg_start(art);
