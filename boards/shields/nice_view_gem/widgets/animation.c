5bb5dbe0c962691b6b1c3fe97ee7526888969e9f
Siddhanta <66236925+siddhantac@users.noreply.github.com>
2025-09-24 14:52:16 +0800

    Arasaka v2 (#2)
    - also added files for corne logo but did not include it in the animation

--- boards/shields/nice_view_gem/widgets/animation.c
+++ boards/shields/nice_view_gem/widgets/animation.c
@@ -2,39 +2,34 @@
 #include <zephyr/kernel.h>
 #include "animation.h"
 
-LV_IMG_DECLARE(crystal_01);
-LV_IMG_DECLARE(crystal_02);
-LV_IMG_DECLARE(crystal_03);
-LV_IMG_DECLARE(crystal_04);
-LV_IMG_DECLARE(crystal_05);
-LV_IMG_DECLARE(crystal_06);
-LV_IMG_DECLARE(crystal_07);
-LV_IMG_DECLARE(crystal_08);
-LV_IMG_DECLARE(crystal_09);
-LV_IMG_DECLARE(crystal_10);
-LV_IMG_DECLARE(crystal_11);
-LV_IMG_DECLARE(crystal_12);
-LV_IMG_DECLARE(crystal_13);
-LV_IMG_DECLARE(crystal_14);
-LV_IMG_DECLARE(crystal_15);
-LV_IMG_DECLARE(crystal_16);
-
-
 LV_IMG_DECLARE(arasaka);
-LV_IMG_DECLARE(arasaka_glitch_1);
-LV_IMG_DECLARE(arasaka_glitch_2);
+// LV_IMG_DECLARE(arasaka_glitch_1);
+// LV_IMG_DECLARE(arasaka_glitch_2);
+// LV_IMG_DECLARE(arasaka_glitch_3);
+// LV_IMG_DECLARE(arasaka_glitch_4);
+LV_IMG_DECLARE(arasaka_glitch_h1);
+LV_IMG_DECLARE(arasaka_glitch_h2);
+
+LV_IMG_DECLARE(corne);
 
 const lv_img_dsc_t *arasaka_anim[] = {
-    &arasaka, &arasaka, &arasaka, &arasaka, 
-    &arasaka, &arasaka, &arasaka, &arasaka_glitch_1, 
-    &arasaka_glitch_2, &arasaka, &arasaka, &arasaka,
-    &arasaka, &arasaka, &arasaka, &arasaka, 
-};
-
-const lv_img_dsc_t *anim_imgs[] = {
-    &crystal_01, &crystal_02, &crystal_03, &crystal_04, &crystal_05, &crystal_06,
-    &crystal_07, &crystal_08, &crystal_09, &crystal_10, &crystal_11, &crystal_12,
-    &crystal_13, &crystal_14, &crystal_15, &crystal_16,
+    &arasaka, &arasaka,
+    &arasaka, &arasaka,
+    &arasaka, &arasaka,
+    &arasaka, &arasaka,
+    &arasaka, &arasaka,
+    &arasaka, &arasaka,
+    &arasaka, &arasaka,
+    // &arasaka_glitch_1, 
+    // &arasaka_glitch_2, 
+    // &arasaka_glitch_3, 
+    // &arasaka_glitch_4, 
+    &arasaka, &arasaka,
+    &arasaka, &arasaka,
+    &arasaka, &arasaka,
+    &arasaka, &arasaka,
+    &arasaka_glitch_h1, 
+    &arasaka_glitch_h2, 
 };
 
 void draw_animation(lv_obj_t *canvas) {
@@ -42,8 +37,7 @@ void draw_animation(lv_obj_t *canvas) {
     lv_obj_t *art = lv_animimg_create(canvas);
     lv_obj_center(art);
 
-    // lv_animimg_set_src(art, (const void **)anim_imgs, 16);
-    lv_animimg_set_src(art, (const void **)arasaka_anim, 16);
+    lv_animimg_set_src(art, (const void **)arasaka_anim, 24);
     lv_animimg_set_duration(art, CONFIG_NICE_VIEW_GEM_ANIMATION_MS);
     lv_animimg_set_repeat_count(art, LV_ANIM_REPEAT_INFINITE);
     lv_animimg_start(art);
@@ -55,7 +49,7 @@ void draw_animation(lv_obj_t *canvas) {
     // srand(k_uptime_get_32());
     // int random_index = rand() % length;
 
-    lv_img_set_src(art, &arasaka);
+    lv_img_set_src(art, &corne);
 #endif
 
     lv_obj_align(art, LV_ALIGN_TOP_LEFT, 0, 0);
